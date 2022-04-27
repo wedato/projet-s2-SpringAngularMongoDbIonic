92adaf70a5359dfb41e43bfd6a0b345c1101377c
JonathanBaltaci <genzio775@gmail.com>
2022-04-27 14:35:07 +0200

    on enleve les custom exception handling et custom httpresponse c'était une mauvaise idéee, plus besoin depuis spring5 avec ResponsestatusException

--- back/src/main/java/com/example/projetsem2qrcode/controlleradmin/User2Controller.java
+++ back/src/main/java/com/example/projetsem2qrcode/controlleradmin/User2Controller.java
@@ -2,7 +2,6 @@ package com.example.projetsem2qrcode.controlleradmin;
 
 
 import com.example.projetsem2qrcode.exceptions.EmailExistException;
-import com.example.projetsem2qrcode.exceptions.ExceptionHandling;
 import com.example.projetsem2qrcode.exceptions.UserNotFoundException;
 import com.example.projetsem2qrcode.exceptions.UsernameExistException;
 import com.example.projetsem2qrcode.modele.User;
@@ -13,12 +12,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
+
+// format date indice?
+//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss" ,timezone = "Europe/Paris")
 
 @RestController
 @RequestMapping(value="/user")
-public class User2Controller extends ExceptionHandling {
+public class User2Controller  {
 
     private UserService userService;
 
@@ -28,9 +31,20 @@ public class User2Controller extends ExceptionHandling {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
-        User newUser = userService.register(user.getFirstName(),user.getLastName(),user.getUsername(),user.getEmail());
-        return new ResponseEntity<>(newUser, OK);
+    public ResponseEntity<User> register(@RequestBody User user)  {
+
+
+        try {
+           User newUser = userService.register(user.getFirstName(),user.getLastName(),user.getUsername(),user.getEmail());
+            return new ResponseEntity<>(newUser, OK);
+        } catch (UserNotFoundException e) {
+            throw new ResponseStatusException(NOT_FOUND, "User not found");
+        } catch (EmailExistException e) {
+            throw new ResponseStatusException(CONFLICT,"Email already exist");
+        } catch (UsernameExistException e) {
+            throw new ResponseStatusException(CONFLICT,"Username already exist");
+        }
+
 
 
     }
--- back/src/main/java/com/example/projetsem2qrcode/exceptions/UsernameExistException.java
+++ back/src/main/java/com/example/projetsem2qrcode/exceptions/UsernameExistException.java
@@ -2,5 +2,6 @@ package com.example.projetsem2qrcode.exceptions;
 
 public class UsernameExistException extends Exception {
     public UsernameExistException(String message) {
+        super(message);
     }
 }
