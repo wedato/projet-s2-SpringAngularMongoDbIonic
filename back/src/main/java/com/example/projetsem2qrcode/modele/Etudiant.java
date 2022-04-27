6d0c7b9354b75ec4e6ea18f09dbcce03c5bbe073
o2163453 <clement.guyot@etu.univ-orleans.fr>
2022-04-27 16:20:05 +0200

    etudiant modifier

--- back/src/main/java/com/example/projetsem2qrcode/modele/Etudiant.java
+++ back/src/main/java/com/example/projetsem2qrcode/modele/Etudiant.java
@@ -1,15 +1,19 @@
 package com.example.projetsem2qrcode.modele;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "etudiants")
+@AllArgsConstructor
 @Getter
 @Setter
 public class Etudiant {
     @Id
+    @JsonIgnore
     private String id;
     private String nom;
     private String prenom;
@@ -17,12 +21,4 @@ public class Etudiant {
     private String groupeTp;
     private boolean emargement;
 
-    public Etudiant() {
-    }
-
-    public Etudiant(String nom, String prenom, String numEtudiant) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.numEtudiant = numEtudiant;
-    }
 }
