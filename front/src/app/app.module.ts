bc8179ef171a5ba61393d48978622c0c6f57ec58
JonathanBaltaci <genzio775@gmail.com>
2022-04-29 19:11:40 +0200

    Angular : mise en place authentication FRONT : authentication.service

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
-import { HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from "./app-routing.module";
+
 import {FormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatCardModule} from "@angular/material/card";
+
 import { AddEtudiantComponent } from './components/etudiant/add-etudiant/add-etudiant.component';
 import { EtudiantDetailsComponent } from './components/etudiant/etudiant-details/etudiant-details.component';
 import { EtudiantsListComponent } from './components/etudiant/etudiants-list/etudiants-list.component';
-import {AppRoutingModule} from "./app-routing.module";
+
 import { CoursListComponent } from './components/cours/cours-list/cours-list.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { GroupeTpComponent } from './groupe-tp/groupe-tp.component';
@@ -36,14 +32,7 @@ import { GroupeTpComponent } from './groupe-tp/groupe-tp.component';
         RouterModule,
         BrowserModule,
         HttpClientModule,
-        BrowserAnimationsModule,
         FormsModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        MatCardModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatExpansionModule,
         AppRoutingModule
     ],
   providers: [],
