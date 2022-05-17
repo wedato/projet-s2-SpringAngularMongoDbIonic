5046c2d81a86de6889c51f88c3b9df13e1fa3189
peredalouis <louis.pereda@etu.univ-orleans.fr>
2022-05-17 11:45:15 +0200

    test Cours

--- back/src/test/java/com/example/projetsem2qrcode/service/CoursServiceTest.java
+++ back/src/test/java/com/example/projetsem2qrcode/service/CoursServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.projetsem2qrcode.service;
 
 import com.example.projetsem2qrcode.exceptions.CoursDejaCreerException;
+import com.example.projetsem2qrcode.exceptions.CoursInnexistantException;
 import com.example.projetsem2qrcode.modele.Cours;
+import com.example.projetsem2qrcode.modele.Prof;
 import com.example.projetsem2qrcode.repository.CoursRepository;
 import com.example.projetsem2qrcode.repository.GroupeTpRepository;
 import com.example.projetsem2qrcode.repository.ProfRepository;
@@ -11,12 +13,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ContextConfiguration(classes = {CoursService.class})
 @ExtendWith(MockitoExtension.class)
 class CoursServiceTest {
+
+    @Autowired
+    private CoursService coursService;
 
     private CoursService underTest;
 
@@ -30,7 +42,7 @@ class CoursServiceTest {
 
     @BeforeEach
     void setUp() {
-        underTest  = new CoursService(coursRepository,groupeTpRepository,profRepository);
+        underTest = new CoursService(coursRepository, groupeTpRepository, profRepository);
     }
 
     @Test
@@ -57,28 +69,50 @@ class CoursServiceTest {
 //        Assertions.assertDoesNotThrow(()->this.instance.createCours(cours));
     }
 
+    /**
+     * Method under test: {@link CoursService#findByNomDuCours(String)}
+     */
     @Test
-    void findByNomDuCours() {
+    void testFindByNomDuCoursok() throws CoursInnexistantException {
+        // given
+        Cours cours = new Cours();
+        cours.setHeureDebut(null);
+        cours.setHeureFin(null);
+        cours.setId("42");
+        cours.setLesGroupes(new HashSet<>());
+        cours.setNom("Nom");
+        cours.setProf(new Prof("42", "Nom", "Prenom", new HashSet<>()));
+        Optional<Cours> ofResult = Optional.of(cours);
+        //when
+        when(this.coursRepository.findCoursByNom((String) any())).thenReturn(ofResult);
+        //assert
+        assertSame(cours, this.coursService.findByNomDuCours("Nom Cours"));
+        verify(this.coursRepository).findCoursByNom((String) any());
     }
 
+    /**
+     * Method under test: {@link CoursService#findByNomDuCours(String)}
+     */
     @Test
-    void deleteCourByNom() {
+    void testFindByNomDuCoursKo1() throws CoursInnexistantException {
+        // when
+        when(this.coursRepository.findCoursByNom((String) any())).thenReturn(Optional.empty());
+        //assert
+        assertThrows(CoursInnexistantException.class, () -> this.coursService.findByNomDuCours("Nom Cours"));
+        verify(this.coursRepository).findCoursByNom((String) any());
     }
 
+    /**
+     * Method under test: {@link CoursService#deleteAllCours()}
+     */
     @Test
-    void getAllCours() {
-        //String nom = "Pereda";
+    void testDeleteAllCours() {
+        //when
+        doNothing().when(this.coursRepository).deleteAll();
+        this.coursService.deleteAllCours();
+        //asser
+        verify(this.coursRepository).deleteAll();
+        assertTrue(this.coursService.getAllCours().isEmpty());
     }
 
-    @Test
-    void updateCours() {
-    }
-
-    @Test
-    void addGroupeTPAuCours() {
-    }
-
-    @Test
-    void addProfAuCours() {
-    }
 }
